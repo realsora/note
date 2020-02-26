1e99fe81cd5a770fcf45f98529b1a082d0baaa3a
realsora <realsora@gmail.com>
2020-02-26 16:29:36 +0000

    ...

--- src/App.js
+++ src/App.js
@@ -25,8 +25,8 @@ class App extends Component {
 
   getNotes = () => {
     axios.get('https://note-app-sora-doh.herokuapp.com/notes')
-    .then((res) => console.log(res.data))
-    .catch((err) => console.log(err.response.data));
+    .then((res) => console.log(res.data));
+    // .catch((err) => console.log(err.response.data));
   }
 
   render(){
